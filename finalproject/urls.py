4b5f70125c839c6389860d5bd169f6938f174249
infinity <88617484+Exiinfinity@users.noreply.github.com>
2022-05-02 18:40:46 +0900

    2020/05/02

--- finalproject/urls.py
+++ finalproject/urls.py
@@ -1,31 +1,11 @@
-"""finalproject URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
-from .views import home, register,plantmanage
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),    
-    # path('users/', include('users.urls')), # users>urls.py에서 관리할거야
-    path('', home),
-    path('register/', register),
-    path('main/', include('weather_app.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('', include('weather_app.urls')),
     path('plantinfo/', views.plantinfo, name="plantinfo"),
     path('plantmanage/', views.plantmanage, name="plantmanage"),
     path('plantrecog/', views.plantrecog, name="plantrecog"),
--- finalproject/views.py
+++ finalproject/views.py
@@ -1,47 +1,43 @@
-from django.shortcuts import render
-def home(request):
-    context={
-    }
-    return render(request, 'main.html', context)
-
-def register(request):
-    context={
-    }
-    return render(request, 'register.html', context)
-
-def main(request):
-    context={
-    }
-    return render(request, 'main.html', context)
+from django.shortcuts import render, redirect
+from django.utils import timezone, dateformat
+from finalproject.models import *
 
 def plantinfo(request):
-    context={
-    }
-    return render(request, 'plantinfo.html', context)
+    if request.method == "GET":
+        user = request.user
+        userid = AuthUser.objects.filter(username=user).values("id")[0]["id"]
+        obj = Plantmanage.objects.filter(username=userid)
+        return render(request, 'plantinfo.html', {"obj": obj})
+    elif request.method == "POST":
+        now = dateformat.format(timezone.localtime(), 'Y-m-d')
+        pid = request.POST["userplantid"]
+        obj = Plantmanage.objects.filter(id=pid).update(waterdate=now)
+        return redirect("/plantinfo")
+    return redirect("/plantinfo")
 
 def plantmanage(request):
-    context={
-    }
-    return render(request, 'plantmanage.html', context)
+    if request.method == "GET":
+        return render(request, "plantmanage.html")
+    elif request.method == "POST":
+        plant_name = request.POST["plant_name"]
+        plant_nickname = request.POST["plant_nickname"]
+        meet_date = request.POST["plant_date"]
+        water_date = request.POST["water_date"]
+        user = request.user
+
+        plant_id = Plants.objects.get(name=plant_name)
+        user = AuthUser.objects.get(username=user)
+        pmanage = Plantmanage(username=user, plant=plant_id, nickname=plant_nickname, meetdate=meet_date, waterdate=water_date)
+        pmanage.save()
+        return redirect("/plantinfo")
+
+    return redirect("/plantinfo")
 
 def plantrecog(request):
-    context={
-    }
-    return render(request, 'plantrecog.html', context)
+    return render(request, 'plantrecog.html')
 
 def plantdisease(request):
-    context={
-    }
-    return render(request, 'plantdisease.html', context)
+    return render(request, 'plantdisease.html')
 
 def plantrecog(request):
-    context={
-    }
-    return render(request, 'plantrecog.html', context)
-
-def search(request):
-    context={
-    }
-    return render(request, 'search.html', context)
-
-
+    return render(request, 'plantrecog.html')
